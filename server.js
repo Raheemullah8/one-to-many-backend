c71c0ada40fd31477514de04e674d94c6e727343
Raheem Ullah He/Him 💻 <154120701+Raheemullah8@users.noreply.github.com>
2026-01-13 15:14:22 +0500

    .

--- server.js
+++ server.js
@@ -82,7 +82,11 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+
+
+if (process.env.NODE_ENV !== "production") {
+ const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 module.exports = app;
